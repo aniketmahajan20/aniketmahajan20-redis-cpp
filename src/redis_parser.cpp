4f067ecf83de86b4b8e1c83b38c03e94c2f8bdee
Aniket <aniketmahajan20@gmail.com>
2024-11-11 15:34:14 -0800

    added expiry support for get and set commands

--- src/redis_parser.cpp
+++ src/redis_parser.cpp
@@ -1,9 +1,13 @@
 #include <sstream>
 #include <stdexcept>
 #include <iostream>
+#include <chrono>
+#include <unistd.h>
+
 #include "redis_parser.hpp"
 
-std::string RedisParser::parseRESPCommand(const std::string& input) {
+
+std::string RedisParser::parseRESPCommand(const std::string& input, std::mutex& database_mutex) {
     size_t pos = 0;
 
     // Parse array indicator, e.g., "*2\r\n"
@@ -14,6 +18,7 @@ std::string RedisParser::parseRESPCommand(const std::string& input) {
 
     // Read number of elements
     size_t elements_end = input.find("\r\n", pos);
+    int num_elements = std::stoi(input.substr(pos, elements_end - pos));
     pos = elements_end + 2;
 
     // Parse command bulk string, e.g., "$4\r\nECHO\r\n"
@@ -29,7 +34,7 @@ std::string RedisParser::parseRESPCommand(const std::string& input) {
         return parsePINGCommand(input, pos);
     }
     else if (command == "SET"){
-        return parseSETCommand(input, pos);
+        return parseSETCommand(input, pos, num_elements, database_mutex);
     }
     else if (command == "GET"){
         return parseGETCommand(input, pos);
@@ -44,8 +49,15 @@ std::string RedisParser::parseRESPCommand(const std::string& input) {
 std::string RedisParser::parseGETCommand(const std::string& input, size_t& pos){
     std::string argument = parseBulkString(input, pos);
     if (database.find(argument) != database.end()){
-        std::string response = database[argument];
-        return "$" + std::to_string(response.size()) + "\r\n" + response + "\r\n";
+        DBValue response_struct = database[argument];
+        if (response_struct.expiry_time == 0 || get_current_time_milli() < response_struct.expiry_time){
+            std::string response = response_struct.value;
+            return "$" + std::to_string(response.size()) + "\r\n" + response + "\r\n";
+        }
+        else{
+            database.erase(argument);
+            return "$-1\r\n";
+        }
     }
     else{
         return "$-1\r\n";
@@ -53,10 +65,23 @@ std::string RedisParser::parseGETCommand(const std::string& input, size_t& pos){
 }
 
 //Parses a SET Command
-std::string RedisParser::parseSETCommand(const std::string& input, size_t& pos){
+std::string RedisParser::parseSETCommand(const std::string& input, size_t& pos, 
+                                        int num_elements, std::mutex& database_mutex){
     std::string argument1 = parseBulkString(input, pos);
     std::string argument2 = parseBulkString(input, pos);
-    database[argument1] = argument2;
+    long expiry_time = 0;
+    // TODO: Implement error handling for cases that px was incorrect 
+    // or the time value is not provided
+    // Currently Assumed that PX and expiry time are valid commands provided
+    if (num_elements > 3){
+        std::string expiry_command = parseBulkString(input, pos);
+        std::string expiry_value = parseBulkString(input, pos);
+        auto current_time = get_current_time_milli();
+        expiry_time = current_time + std::stoi(expiry_value);
+    }
+    database_mutex.lock();
+    database[argument1] = DBValue(argument2, expiry_time);
+    database_mutex.unlock();
     return "+OK\r\n";
 }
 
@@ -93,4 +118,11 @@ std::string RedisParser::parseBulkString(const std::string& input, size_t& pos) 
     pos += length + 2; // Move position past the bulk string and trailing "\r\n"
 
     return bulk_string;
+}
+
+inline long long RedisParser::get_current_time_milli(){
+    auto now = std::chrono::system_clock::now();
+    auto duration = now.time_since_epoch();
+    auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
+    return milliseconds;
 }
--- src/redis_parser.hpp
+++ src/redis_parser.hpp
@@ -4,24 +4,37 @@
 
 #include <string>
 #include <unordered_map>
+#include <mutex>
+
+
+struct DBValue {
+    std::string value;
+    long expiry_time;
+    DBValue() : value(""), expiry_time(0) {}
+    DBValue(std::string val) : value(val), expiry_time(0) {}
+    DBValue(std::string val, long time) : value(val), expiry_time(time) {}
+};
 
 class RedisParser {
 public:
     // Parses a Redis command and returns the response
-    std::string parseRESPCommand(const std::string& input);
+    std::string parseRESPCommand(const std::string& input, std::mutex& database_mutex);
 
 private:
-    std::unordered_map<std::string, std::string> database;
+    std::unordered_map<std::string, DBValue> database;
     // Parses a GET Command
     std::string parseGETCommand(const std::string& input, size_t& pos);
     // Parses a SET command
-    std::string parseSETCommand(const std::string& input, size_t& pos);
+    std::string parseSETCommand(const std::string& input, size_t& pos, 
+                                int num_elements, std::mutex& database_mutex);
     // Parses an echo command
     std::string parseECHOCommand(const std::string& input, size_t& pos);
     // Parses an PING command
     std::string parsePINGCommand(const std::string& input, size_t& pos);
     // Parses a bulk string in RESP format, e.g., "$9\r\nraspberry\r\n"
     std::string parseBulkString(const std::string& input, size_t& pos);
+    // Get the current time in milliseconds
+    inline long long get_current_time_milli();
 };
 
 #endif // REDISPARSER_HPP
