793ef5a3de6859d7139630fb1ad66b14830fc619
Aniket <aniketmahajan20@gmail.com>
2024-11-20 13:06:56 -0800

    removed test folder from git cache

--- test/runcpp.sh
+++
@@ -1,2 +0,0 @@
-g++ --std=c++17 -o test_code test_code.cpp
-./test_code aniket mahajan is the best person
--- test/test_code.cpp
+++
@@ -1,7 +0,0 @@
-#include <algorithm>
-#include <iostream>
-
-int main(int argc, char *argv[]) {
-    std::copy(argv, argv + argc, std::ostream_iterator<char *>(std::cout, "\n"));
-    // ...
-}
